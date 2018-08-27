4285a688921f85f3a9f5012af3843782f0164a85
Jennifer Lawson <jenniferlawson000@gmail.com>
2018-08-27 13:11:29 -0500

    final

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random random = new Random();
+            List<int> numbers = new List<int>();
+            List<int> squared = new List<int>();
+            for (int i = 0; i <= 20; i++)
+            {
+                numbers.Add(random.Next(51));
+            }
+            foreach (var number in numbers)
+            {
+                squared.Add(number^2);
+            }
+            foreach (var number in squared)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
